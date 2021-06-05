5051d932de015b38a192dcad6cb413b2b9b5e906
yeswuchaoxin <2115114745@qq.com>
2021-06-05 20:17:41 +0800

    1.0

---
+++ im_bird_sys/src/main/java/org/wdzl/ImBirdSysApplication.java
@@ -0,0 +1,13 @@
+package org.wdzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImBirdSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImBirdSysApplication.class, args);
+	}
+
+}
